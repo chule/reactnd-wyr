064c9d89f87a1d689647c47d27f7cbd422a14c62
Igor Cuckovic <cuckovic@gmail.com>
2018-05-18 14:50:10 +0200

    Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react"
 
 
-const Leaderboard = () => {
+const Leaderboard = ({ users }) => {
+    console.log(users)
     return (
         <div>
-            Leaderboard
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>ID</td>
+                        <td>Score</td>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    {users.map(user => <tr key={user.id} >
+                        <td>{user.name}</td>
+                        <td>{user.id}</td>
+                        <td>{user.score}</td>
+                    </tr>)}
+                </tbody>
+            </table>
         </div>
     )
 };
--- src/containers/Leaderboard.js
+++ src/containers/Leaderboard.js
@@ -5,8 +5,17 @@ import Leaderboard from '../components/Leaderboard';
 function mapStateToProps({ users }) {
 
     return {
-        users
+        users: Object.keys(users)
+            .map(user => {
+                return {
+                    ...users[user],
+                    score: (users[user].questions.length + Object.keys(users[user].answers).length)
+                }
+            })
+            .sort((a, b) => {
+                return b.score - a.score
+            })
     }
-  }
-  
-  export default connect(mapStateToProps)(Leaderboard)
+}
+
+export default connect(mapStateToProps)(Leaderboard)
